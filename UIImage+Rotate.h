3a9a0276f8636e3d626b5ddba538d9f3acf6bafc
mingchen <je-bai@163.com>
2014-06-20 22:33:27 +0800

    add rotate

---
+++ UIImage+Rotate.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Rotate.h
+//  ImageRotate
+//
+//  Created by mingchen on 6/19/14.
+//  Copyright (c) 2014 sencloudx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Rotate)
+
+@end
---
+++ UIImage+Rotate.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Rotate.m
+//  ImageRotate
+//
+//  Created by mingchen on 6/19/14.
+//  Copyright (c) 2014 sencloudx. All rights reserved.
+//
+
+#import "UIImage+Rotate.h"
+
+@implementation UIImage (Rotate)
+
+@end
